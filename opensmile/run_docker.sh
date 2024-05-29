5717bcc12afe611da3bbbe3b82f13cec9e6b4054
Cody Karjadi <ckarjadi@bu.edu>
2024-05-29 16:50:16 -0400

    changed from tabs to spaces

--- opensmile/run_docker.sh
+++ opensmile/run_docker.sh
@@ -2,16 +2,16 @@
 
 if [ $# -eq 1 ];
 then
-	container_name=$1
+    container_name=$1
 else
-	container_name="osm-feat-rhel8"
+    container_name="osm-feat-rhel8"
 fi
 
 if [ $# -eq 2 ];
 then
-	docker_name=$1
+    docker_name=$1
 else
-	docker_name="osm-feat-rhel8-python3"
+    docker_name="osm-feat-rhel8-python3"
 fi
 
 docker run -v $(pwd):/scripts -it --rm --name $container_name $docker_name bash
