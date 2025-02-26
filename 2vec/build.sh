6d3ad2ab9d53d9138645b8bd0d37f72bcb4017d1
Cody Karjadi <ckarjadi@fhs.org>
2025-02-26 16:12:47 -0500

    moved 2vec to audio_embeddings, update README

--- 2vec/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t grip-dockers/builder:latest -f Dockerfile.builder .
-docker build -t grip-dockers/data2vec:latest -f Dockerfile.data2vec . 
-docker build -t grip-dockers/wav2vec:latest -f Dockerfile.wav2vec . 
